2e89c02dae049f5ef2ca2436294e9b688b3e51ea
Nikita Nafranets <eddimensi@gmail.com>
2020-02-02 18:35:52 +0300

    little improve

--- src/feature/checkOnWin.ts
+++ src/feature/checkOnWin.ts
@@ -1,50 +1,46 @@
 import { MAX_STEPS, Players, Steps } from '.';
 import { joinXY, ICoords } from './coords';
 
-const filterStepByPlayer = (steps: Steps, player: Players) =>
-  Object.entries(steps).reduce((acc, [xy, p]) => {
-    if (p !== player) return acc;
-    acc.add(xy);
-    return acc;
-  }, new Set() as Set<string>);
-
-function* generatePoints() {
-  const oX = [0, 1, 1, 1, 0, -1, -1, -1];
-  const oY = [-1, -1, 0, 1, 1, 1, 0, -1];
-  for (let n = 0; n < 8; n++) {
-    yield [oX[n], oY[n]];
-  }
-}
+const roundPoints = [
+  [0, -1],
+  [1, -1],
+  [1, 0],
+  [1, 1],
+  [0, 1],
+  [-1, 1],
+  [-1, 0],
+  [-1, -1],
+];
 
 const createRadius = () => {
   const result: Record<string, number> = {};
-  for (let [x, y] of generatePoints()) {
-    result[joinXY(x, y)] = 1;
+  for (let [x, y] of roundPoints) {
+    result[joinXY(x, y)] = 0;
   }
   return result;
 };
 
-function checkWin(steps: Set<string>, startStep: ICoords) {
-  let counter = 1;
+function checkWin(steps: Steps, startStep: ICoords, player: Players) {
+  let counter = 0;
   const radius = createRadius();
   while (MAX_STEPS > counter) {
-    for (let [x, y] of generatePoints()) {
+    for (let [x, y] of roundPoints) {
       const i = joinXY(x, y);
       const newX = startStep.x + x * counter;
       const newY = startStep.y + y * counter;
-      if (steps.has(joinXY(newX, newY))) {
-        radius[i] += 1;
-      }
+      radius[i] += steps[joinXY(newX, newY)] === player ? 1 : 0;
     }
     counter++;
   }
-  return Math.max(...Object.values(radius)) === MAX_STEPS;
+  const x = radius['1:0'] + radius['-1:0'];
+  const y = radius['0:-1'] + radius['0:1'];
+  const xy = radius['-1:1'] + radius['1:-1'];
+  const yx = radius['1:1'] + radius['-1:-1'];
+  return Math.max(x, y, xy, yx) >= MAX_STEPS - 1;
 }
 
 export function checkOnWin(steps: Steps, newStep: ICoords, player: Players) {
-  console.log(steps, newStep, player);
-  const stepsByPlayer = filterStepByPlayer(steps, player);
-  if (checkWin(stepsByPlayer, newStep)) {
+  if (checkWin(steps, newStep, player)) {
     return player;
   }
   return -1;
